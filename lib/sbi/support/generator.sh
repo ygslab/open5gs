964cf40ece33b81b3a3f495795ad16fd80faa539
Sukchan Lee <acetcom@gmail.com>
2020-05-03 14:16:45 -0400

    Add dependency ulfius and libcurl

--- lib/sbi/support/generator.sh
+++ lib/sbi/support/generator.sh
@@ -4,4 +4,4 @@ export C_POST_PROCESS_FILE="/usr/bin/uncrustify --no-backup"
 export UNCRUSTIFY_CONFIG=../.openapi-generator/uncrustify.cfg
 openapi_generator_cli="java -jar ${HOME}/Documents/git/my/openapi-generator/modules/openapi-generator-cli/target/openapi-generator-cli.jar"
 
-$openapi_generator_cli generate -i ./modified/TS29510_Nnrf_NFManagement.yaml -c ../.openapi-generator/config.yaml -g c -o ..
+$openapi_generator_cli generate -i ./modified/TS29510_Nnrf_NFManagement.yaml -c ../.openapi-generator/config.yaml -g go-experimental -o ~/go-experimental
