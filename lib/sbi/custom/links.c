f7e3d662ae09d0cbbbffa715c9d6b17f41c47bd8
Sukchan Lee <acetcom@gmail.com>
2020-05-18 22:46:01 -0400

    update it

--- lib/sbi/custom/links.c
+++ lib/sbi/custom/links.c
@@ -6,7 +6,7 @@
 
 cJSON *ogs_sbi_links_convertToJSON(ogs_sbi_links_t *links)
 {
-    cJSON *rootJSON = NULL;
+    cJSON *root = NULL;
     cJSON *linksJSON = NULL;
     cJSON *itemsJSON = NULL;
     cJSON *selfJSON = NULL;
@@ -15,23 +15,30 @@ cJSON *ogs_sbi_links_convertToJSON(ogs_sbi_links_t *links)
     ogs_assert(links);
     ogs_assert(links->self);
 
-    rootJSON = cJSON_CreateObject();
-    ogs_assert(rootJSON);
+    root = cJSON_CreateObject();
+    ogs_assert(root);
 
-    linksJSON = cJSON_AddObjectToObject(rootJSON, "_links");
+    linksJSON = cJSON_AddObjectToObject(root, "_links");
     ogs_assert(linksJSON);
 
-    itemsJSON = cJSON_AddObjectToObject(linksJSON, "items");
+    itemsJSON = cJSON_AddArrayToObject(linksJSON, "items");
     ogs_assert(itemsJSON);
 
     OpenAPI_list_for_each(links->items, node) {
+        cJSON *object = NULL;
+
         if (!node->data) continue;
-        cJSON_AddStringToObject(itemsJSON, "href", node->data);
+
+        object = cJSON_CreateObject();
+        ogs_assert(object);
+
+        cJSON_AddItemToObject(object, "href", cJSON_CreateString(node->data));
+        cJSON_AddItemToArray(itemsJSON, object);
     }
 
     selfJSON = cJSON_AddObjectToObject(linksJSON, "self");
     ogs_assert(selfJSON);
     cJSON_AddStringToObject(selfJSON, "href", links->self);
 
-    return rootJSON;
+    return root;
 }
